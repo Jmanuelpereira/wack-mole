e49456d212e8f9f0bd7901a1261e5183e40ad9ad
Jose Pereira <jmanuelpereira2@gmail.com>
2019-11-17 10:36:03 -0500

    Adding mole image and other features

--- script.js
+++ script.js
@@ -1,41 +1,125 @@
-console.log('hello');
 
 window.onload = function(){
+
+    let user = this.prompt("Please enter your name", "John Doe")
+
+
+    document.getElementById("user").innerHTML = " " +user;
     
-    let mole = document.createElement('div')
-    mole.classList.add('mole');
+    // let mole = document.createElement('div')
+    // mole.classList.add('mole');
+
+    let mole = document.createElement('img');
+    mole.setAttribute('src', "./topo-png.png");
+    mole.style.width = "150px";
     
     
     let timeMole = 1000;
     let moleGolpeado = true;
-    let score = 0;
+    let score = 10;
+
+    let dificultad = document.querySelectorAll(".btn")
+
+    dificultad.onclick = () => {
+        if(level.innerHTML === "Easy")
+        return 6500
+    }
+
+    // dificultad.forEach((level)=>{
+
+    //     level.onclick = () => {
+    //         if(level.innerHTML === "Easy")
+    //         return 6500
+    //     }
+
+
+        
+     
+
+    // })
+
+
+
+    // dificultad.onclick = function getDificultad() {
+
+    //     switch(level.innerHTML){
+
+    //         case 'Easy':
+    //         level.innerHTML = 6500;
+    //         break;
+
+    //         case 'Moderate':
+    //         level.innerHTML = 3000;
+    //         break;
+                
+
+    //         case 'Hard':
+    //         level.innerHTML = 500;
+    //         break;
+            
+    //     }
+
+    //    if(level.innerHTML === 6500)
+    //    return 6500
+
+    //    if(level.innerHTML === 3000)
+    //    return 3000
+
+    //    if(level.innerHTML === 6500)
+    //    return 6500
+
+
+
+
+    // }
+
+  
+
+    function missMole(){
+
+        if(!moleGolpeado){
+            score -= 10
+       }
+       updateScore()
+       mole.remove()
+    }
+
+   
 
     function getAMole() {
         
             setInterval(function(){
 
-            if(!moleGolpeado){
-                score -= 10
-            }
-
-            timeMole = Math.floor(Math.random() * 1000);
-
-            let index = Math.floor(Math.random() * document.querySelectorAll('.col').length)
             
-
+                missMole();
+                
+                
+                timeMole = Math.floor(Math.random() * 3500);
+                let index = Math.floor(Math.random() * document.querySelectorAll('.col').length)
+                
+                
                 document.querySelectorAll('.col'+index).forEach(function(coll){
                     
-                coll.appendChild(mole)    
+                    coll.append(mole) 
+                    moleGolpeado = false;  
+                    
+                })
+                
             
-            })
+                
+            }, timeMole);
 
             
-            }, timeMole, timeMole);
+            
     }
 
-    getAMole()
+    let started = document.querySelector(".start");
+
+    started.onclick = function(){
+
+        getAMole()
+    }
      
-     //let columna =  document.querySelectorAll('.col')
      
      mole.onclick = function(){   
          
@@ -43,44 +127,34 @@ window.onload = function(){
          mole.remove()
          moleGolpeado = true;
          
-         updateScore()
+         
      }
     
 function updateScore() {
     
     document.getElementById("score").innerHTML = score
+    showStatus();
     
 }
 
+function showStatus(){
 
+    if(score > 100)
+        alert("You Win!!!")
+    
+    if(score <=0){
 
-// columna.forEach(col => {
         
-//     col.onclick = function() {
-//     score -=10
-    
-    
-//     document.getElementById("score").innerHTML = score
-//     }
-// })
+
+         window.location.reload();
+         alert("Youlose!!")
+        
+
+        
+    }
 
 
-
-
-
-   
-
-     
-
-    //  document.querySelectorAll('.col'+index).forEach(function(coll){
-         
-       
-
-
-    //  } .onlick() = function(){
-
-    //     mole.removeChild(div.mole)
-    
+}
 
 
 
